3247e09e95945d85476a348b6d18919a334017ab
Shobhitha-182000 <shobhithaas18@gmail.com>
2023-12-16 12:45:52 +0530

    type  script

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+var number1 = document.getElementById('num1');
+var number2 = document.getElementById('num2');
+var button = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
 console.log(add(1, 2));
-console.log(add('1', '2'));
+button.addEventListener('click', function () {
+    var num1 = parseFloat(number1.value);
+    var num2 = parseFloat(number2.value);
+    // Check if the conversion is successful before proceeding
+    if (!isNaN(num1) && !isNaN(num2)) {
+        var result = add(num1, num2);
+        console.log(result);
+    }
+    else {
+        console.error("Invalid input. Please enter valid numbers.");
+    }
+});
--- app.ts
+++ app.ts
@@ -1,5 +1,22 @@
-function add(num1,num2){
-    return num1+num2;
+const number1 = document.getElementById('num1') as HTMLInputElement;
+const number2 = document.getElementById('num2') as HTMLInputElement;
+const button = document.querySelector('button');
+
+function add(num1: number, num2: number): number {
+    return num1 + num2;
 }
-console.log(add(1,2));
-console.log(add('1','2'));
+
+ 
+
+button.addEventListener('click', () => {
+    const num1 = parseFloat(number1.value);
+    const num2 = parseFloat(number2.value);
+
+    // Check if the conversion is successful before proceeding
+    if (!isNaN(num1) && !isNaN(num2)) {
+        const result = add(num1, num2);
+        console.log(result);
+    } else {
+        console.error("Invalid input. Please enter valid numbers.");
+    }
+});
